c771e72ba1b6e41c9b89eba5ec7fa7fbc610de69
Yi Wang <yi.wang@publicissapient.com>
2023-03-29 14:25:31 -0400

    food order app

---
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../../store/cart-context";
+import { MealItemForm } from "./MealItemForm";
+
+import classes from "./MealItem.module.css";
+
+export const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.desciption}>{props.desciption}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
---
+++ food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import { AvailableMeals } from "./AvailableMeals";
+import { MealsSummary } from "./MealsSummary";
+
+export const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
