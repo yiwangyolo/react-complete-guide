f1dfe134dd4b253a43ebe8cb053af05ac1f1c7d2
Yi Wang <yi.wang@publicissapient.com>
2023-04-24 15:33:50 -0400

    more redux implement

---
+++ redux-demo-3/src/components/Cart/CartItem.js
@@ -0,0 +1,40 @@
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+  const { id, title, quantity, total, price } = props.item;
+
+  const onRemoveHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const onAddHandler = () => {
+    dispatch(cartActions.addItemToCart({ id, title, price }));
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={classes.price}>
+          ${total.toFixed(2)}{" "}
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={onRemoveHandler}>-</button>
+          <button onClick={onAddHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
